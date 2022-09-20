a13580c59d57160861a90e55812dfe570a918e01
Baris Eser <bariseser@gmail.com>
2022-09-20 14:46:53 +0300

    add kibana and elastic

--- app/main.go
+++ app/main.go
@@ -18,7 +18,17 @@ func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		io.WriteString(w, "Pong")
-		logger.Printf("%s - %s - %s - %d", html.EscapeString(r.Method), html.EscapeString(r.URL.Path), r.RemoteAddr, http.StatusOK)
+		logger.Printf("- %s - %s - %s - %d", html.EscapeString(r.Method), html.EscapeString(r.URL.Path), r.RemoteAddr, http.StatusOK)
+	})
+	http.HandleFunc("/bad-gateway", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusBadGateway)
+		io.WriteString(w, "Bad Gateway")
+		logger.Printf("- %s - %s - %s - %d", html.EscapeString(r.Method), html.EscapeString(r.URL.Path), r.RemoteAddr, http.StatusBadGateway)
+	})
+	http.HandleFunc("/move", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusMovedPermanently)
+		io.WriteString(w, "Moved Permanently")
+		logger.Printf("- %s - %s - %s - %d", html.EscapeString(r.Method), html.EscapeString(r.URL.Path), r.RemoteAddr, http.StatusMovedPermanently)
 	})
 	log.Fatalln(http.ListenAndServe(":9090", nil))
 }
